d140f109e9fe3c36047c5651641d15d209002761
angelpineda-dev <angeljpa95@gmail.com>
2024-12-09 10:12:24 -0600

    booking controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\BookingStatus;
 use App\Helpers\ApiResponse;
 use App\Models\Booking;
 use App\Services\BookingService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Validator;
 
 class BookingController extends Controller
@@ -35,36 +38,108 @@ class BookingController extends Controller
     }
 
     /**
-     * validar rango de fechas
+     * //validar rango de fechas
+     * //crear booking
      * crear payment_intent
-     * crear booking
      * regresar client_secret
      */
 
-    $roomID = $request->input('room_id');
-    $initialDate = $request->input('check_in');
-    $finalDate = $request->input('check_out');
+    $roomID = $request->input(key: 'room_id');
+    $initialDate = $request->input(key: 'check_in');
+    $finalDate = $request->input(key: 'check_out');
 
     try {
 
-      // todo: create service
+      // todo: agregar rollbacks
 
-      $querySearchBooking = Booking::query()
-        ->where('room_id', $roomID);
+      $querySearchBooking = Booking::query()->where(column: 'room_id', operator: $roomID);
 
-      $overlap = $this->bookingService->checkOverlap($querySearchBooking, $initialDate, $finalDate);
-
-
+      $overlap = $this->bookingService->checkOverlap(query: $querySearchBooking, initialDate: $initialDate, finalDate: $finalDate);
 
       if ($overlap) {
-        return ApiResponse::error('Duplicated booking');
+        return ApiResponse::error('Duplicated booking dates');
       }
 
       $booking = Booking::create($request->all());
 
       return ApiResponse::success(data: $booking, message: 'Todo ok');
     } catch (\Exception $e) {
-      return ApiResponse::error('Unexpected error', $e->getMessage());
+
+      return ApiResponse::error(message: 'Not expected error ', errors: $e->getMessage(), code: Response::HTTP_INTERNAL_SERVER_ERROR);
     }
   }
+
+  public function index(Request $request)
+  {
+    $page = $request->query(key: 'page', default: 1);
+    $perPage = $request->query(key: 'per_page', default: 10);
+    $status = $request->query(key: 'status', default: "0");
+    $search = $request->query(key: 'search', default: '');
+
+    try {
+
+      $query = Booking::query()->where(column: 'status', operator: $status);
+
+      if ($search) {
+        $query->where(column: 'name', operator: $search);
+      }
+
+      $data = $this->paginateData(query: $query, perPage: $perPage, page: $page);
+
+      return ApiResponse::success(data: $data);
+
+    } catch (\Exception $e) {
+
+      return ApiResponse::error(message: 'Not expected error ', errors: $e->getMessage(), code: Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public function show(string $id)
+  {
+    try {
+      $booking = Booking::findOrFail($id);
+
+      return ApiResponse::success($booking);
+
+    } catch (ModelNotFoundException $e) {
+
+      return ApiResponse::error(message: 'Resource not found ', errors: $e->getMessage(), code: Response::HTTP_NOT_FOUND);
+    } catch (\Exception $e) {
+
+      return ApiResponse::error(message: 'Not expected error ', errors: $e->getMessage(), code: Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public function update(string $id)
+  {
+    try {
+      $booking = Booking::findOrFail(id: $id);
+
+      return ApiResponse::success(data: $booking);
+
+    } catch (ModelNotFoundException $e) {
+
+      return ApiResponse::error(message: 'Resource not found ', errors: $e->getMessage(), code: Response::HTTP_NOT_FOUND);
+    } catch (\Exception $e) {
+
+      return ApiResponse::error(message: 'Not expected error ', errors: $e->getMessage(), code: Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public function delete(string $id)
+  {
+    try {
+      $booking = Booking::findOrFail($id);
+
+      return ApiResponse::success($booking);
+
+    } catch (ModelNotFoundException $e) {
+
+      return ApiResponse::error(message: 'Resource not found ', errors: $e->getMessage(), code: Response::HTTP_NOT_FOUND);
+    } catch (\Exception $e) {
+
+      return ApiResponse::error(message: 'Not expected error ', errors: $e->getMessage(), code: Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+  }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -12,24 +12,8 @@ use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\SizeController;
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum'); */
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('refresh', [AuthController::class, 'refresh']);
-
-Route::get('service', [ServiceController::class, 'index']);
-Route::get('service/{id}', [ServiceController::class, 'show']);
-
-Route::get('size', [SizeController::class, 'index']);
-Route::get('size/{id}', [SizeController::class, 'show']);
-
-Route::get('floor', [FloorController::class, 'index']);
-Route::get('floor/{id}', [FloorController::class, 'show']);
-
-Route::get('season', [SeasonController::class, 'index']);
-Route::get('season/{id}', [SeasonController::class, 'show']);
 
 Route::get('room', [RoomController::class, 'index']);
 Route::get('room/{id}', [RoomController::class, 'show']);
@@ -38,8 +22,6 @@ Route::get('image', [ImageController::class, 'index']);
 Route::get('image/{id}', [ImageController::class, 'show']);
 
 Route::post('booking', [BookingController::class, 'store']);
-
-
 
 Route::group([
   'middleware' => 'api',
@@ -54,20 +36,27 @@ Route::group([
   Route::put('role/{role}', [RoleController::class, 'update'])->middleware('role:manager');
   Route::delete('role/{role}', [RoleController::class, 'delete'])->middleware('role:manager');
 
-
+  Route::get('service', [ServiceController::class, 'index'])->middleware(['role:manager|admin']);
+  Route::get('service/{id}', [ServiceController::class, 'show'])->middleware(['role:manager|admin']);
   Route::post('service', [ServiceController::class, 'store'])->middleware(['role:manager|admin']);
   Route::put('service/{id}', [ServiceController::class, 'update'])->middleware(['role:manager|admin']);
   ;
   Route::delete('service/{id}', [ServiceController::class, 'delete'])->middleware(['role:manager|admin']);
 
+  Route::get('size', [SizeController::class, 'index'])->middleware(['role:manager|admin']);
+  Route::get('size/{id}', [SizeController::class, 'show'])->middleware(['role:manager|admin']);
   Route::post('size', [SizeController::class, 'store'])->middleware(['role:manager|admin']);
   Route::put('size/{id}', [SizeController::class, 'update'])->middleware(['role:manager|admin']);
   Route::delete('size/{id}', [SizeController::class, 'delete'])->middleware(['role:manager|admin']);
 
+  Route::get('floor', [FloorController::class, 'index'])->middleware(['role:manager|admin']);
+  Route::get('floor/{id}', [FloorController::class, 'show'])->middleware(['role:manager|admin']);
   Route::post('floor', [FloorController::class, 'store'])->middleware(['role:manager|admin']);
   Route::put('floor/{id}', [FloorController::class, 'update'])->middleware(['role:manager|admin']);
   Route::delete('floor/{id}', [FloorController::class, 'delete'])->middleware(['role:manager|admin']);
 
+  Route::get('season', [SeasonController::class, 'index'])->middleware(['role:manager|admin']);
+  Route::get('season/{id}', [SeasonController::class, 'show'])->middleware(['role:manager|admin']);
   Route::post('season', [SeasonController::class, 'store'])->middleware(['role:manager|admin']);
   Route::put('season/{id}', [SeasonController::class, 'update'])->middleware(['role:manager|admin']);
   Route::delete('season/{id}', [SeasonController::class, 'delete'])->middleware(['role:manager|admin']);
@@ -87,5 +76,9 @@ Route::group([
   Route::delete('price/{id}', [PriceController::class, 'delete'])->middleware(['role:manager|admin']);
 
 
+  Route::get('booking', [BookingController::class, 'index'])->middleware(['role:manager|admin']);
+  Route::get('booking', [BookingController::class, 'show'])->middleware(['role:manager|admin']);
+  Route::put('booking', [BookingController::class, 'update'])->middleware(['role:manager|admin']);
+  Route::delete('booking', [BookingController::class, 'delete'])->middleware(['role:manager|admin']);
 
 });
